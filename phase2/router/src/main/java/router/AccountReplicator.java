dcc19cb373798a24b499eb5dc63d3cdf78e7e14f
JWill2580 <joshwilson2580@gmail.com>
2020-05-05 14:43:08 +1200

    first commit

---
+++ phase2/router/src/main/java/router/AccountReplicator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package router;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ *
+ * @author admin
+ */
+public class AccountReplicator extends RouteBuilder{
+
+    @Override
+    public void configure() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
