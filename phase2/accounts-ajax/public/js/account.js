1638015a10a826f0b3c0b668150f3e75a57d3d1a
JWill2580 <joshwilson2580@gmail.com>
2020-05-07 15:12:02 +1200

    Update

--- phase2/accounts-ajax/public/js/account.js
+++ phase2/accounts-ajax/public/js/account.js
@@ -17,5 +17,6 @@ module.controller('AccountController', function (accountApi) {
     this.test = "It works!";
     this.addAccount = function (accountToAdd) {
         accountApi.save({}, accountToAdd);
+        //console.log(accountToAdd);
     };
 });
